d07331439410e7f216a282d5d186d0ae32767e42
BlueBerry2606 <su8716@srmist.edu.in>
2026-03-17 13:56:29 +0530

    Displaying Hello with command line argument or default message

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,11 @@
 public class HelloApp {
-    public static void main(String[] args){
-        String name = "Shivam";
+    public static void main(String [] args){
 
-        System.out.println("Hello, " + name + "!");
+        String name = "Shivam";
+        if(args.length > 0){
+            name = args[0];
+        }
+
+        System.out.println("Hi, " + name + "!");
     }
 }
